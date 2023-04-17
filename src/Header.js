585909e9c7ec4fad857accbe358eb97337c32b85
Yasmin Ahmed <yaahmeed@icloud.com>
2023-04-17 01:44:08 -0700

    lab1 complete

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-
 class Header extends React.Component{
   render(){
-    return <h1>Horned Beasts</h1>
+    return (
+      <>
+      <h1>Horned Beasts</h1>
+      </>
+    )
   }
-
 }
 
 export default Header;
--- src/Main.js
+++ src/Main.js
@@ -1,20 +1,13 @@
 import React from 'react';
-import Beast from './Beast';
+import HornedBeast from './HornedBeast';
 
-
-class Main extends React.Component{
-  render(){
+class Main extends React.Component {
+  render() {
     return (
-    <>
-      <h3>Hello Class</h3>
-      <Beast name="Jordan" />
-      <Beast name="Ken" />
-      <Beast name="Yaz" />
-      <Beast />
-      <Beast />
-      <Beast />
-    </>
-
+      <>
+        <HornedBeast title="UniWhal" imageUrl="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" description="A unicorn and a narwhal nuzzling their horns" />
+        <HornedBeast title="Rhino Family" imageUrl="https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80" description="Parent rhino with two babies" />
+      </>
     )
   }
 }
